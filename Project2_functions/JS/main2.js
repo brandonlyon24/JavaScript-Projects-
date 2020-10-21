02cab033d2aa9afa2c2670219ea5a06b983efc47
brandonlyon24 <72480983+brandonlyon24@users.noreply.github.com>
2020-10-21 12:55:35 -0700

    Assignment pg 64

---
+++ Project2_functions/JS/main2.js
@@ -0,0 +1,8 @@
+
+function Button_1() {
+    var str = "This text is red";
+    var result = str.fontcolor("red")
+    document.getElementById("Red_text") .innerHTML = result;
+}
+var A = "This is a string"
+document.write(A)
