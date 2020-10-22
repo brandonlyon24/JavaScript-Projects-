fe15f302e4396ada542012c1d12fa37b247e3827
brandonlyon24 <72480983+brandonlyon24@users.noreply.github.com>
2020-10-22 08:50:09 -0700

    New code

--- Project2_functions/Project3_math_operators/JS/main3.js
+++ Project2_functions/Project3_math_operators/JS/main3.js
@@ -1,7 +1,11 @@
-function Math_Function(x, y) {
-var x = 10 ;
-var y = 20;
-return x + y;
-var Math = 10 + 20;
-document.getElementById("Math") .innerHTML = "10 + 20 =" + return;
+function Math_Function() {
+    var x = 10;
+    var y = 20;
+    var math = 10 + 20;
+    document.getElementById("Math").innerHTML = "10 + 20 = " + math;
 }
+function Math_Return_Function() {
+    return 10 + 20;
+}
+var A = Math_Return_Function()) ;
+console.log(Math_Return_Function())
