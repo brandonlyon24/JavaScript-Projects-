c8c1dce71a506bef28bec2f060c7dc77c96a06ce
brandonlyon24 <72480983+brandonlyon24@users.noreply.github.com>
2020-10-22 17:15:50 -0700

    Assignment submission

--- Project2_functions/Project3_math_operators/JS/main.js
+++ Project2_functions/Project3_math_operators/JS/main.js
@@ -5,81 +5,48 @@ function Math_Function() {
     var math = 10 + 20;
     document.getElementById("Math").innerHTML = "10 + 20 = " + math;
 }
-function Math_Return_Function() {
-    return 10 + 20;
-}
-var A = Math_Return_Function() ;
-console.logMath_Return_Function()
 //subtraction function
-
 function Subtraction_Function() {
     var Subtraction = 5 - 2;
-    document.getElementById("Math").innerHTML = "5 - 2" + Subtraction;
+    document.getElementById("Subtraction").innerHTML = "5 - 2 = " + Subtraction;
 }
-function Subtraction_Return_Function() {
-    return 5 - 2;
-}
-
-var B = Subtraction_Function();
-console.logMath_Return_Subtraction()
 //multiplication function
-
 function Multiplication() {
     var simple_Math = 6 * 8;
-    document.getElementById("Math").innerHTML = "6 x 8 = " + simple_Math;
+    document.getElementById("Multiplication").innerHTML = "6 x 8 = " + simple_Math;
 }
-function Multiplication_Return_Function(){
-    return 6 * 8;
-}
-
 //division function
-
 function Division() {
     var simple_Math = 48 / 6;
-    document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+    document.getElementById("Division").innerHTML = "48 / 6 = " + simple_Math;
 }
-function Division_Return_Function() {
-    return 48 / 6;
-}
-
 //multiple operations
-
 function more_Math() {
     var simple_Math = (1 + 2) * 10 / 2 - 5;
-    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+    document.getElementById("more_Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
 }
-function more_Math_Return_Function() {
-    return (1 + 2) * 10 / 2 - 5; 
-}
-
 //modulus operator function
 function modulus_Operator() {
     var simple_Math = 50 % 3;
-    document.getElementById("Math").innerHTML="When you divide 50 by 3 you have a remainder of: "+simple_Math;
+    document.getElementById("modulus_Operator").innerHTML="When you divide 50 by 3 you have a remainder of: "+simple_Math;
 }
-function modulus_Operator_Return_Function() {
-    return  50 % 3;
-}
-
 //Unary operator 
 function negation_Operator() {
     var X = 10;
-    document.getElementById("Math").innerHTML=-x;
+    document.getElementById("negation_Operator").innerHTML=--X;
 }
-function negation_Operator_Return_Function() {
-    return -10;
-}
-
 function increment_Operator() {
 var W = 5;
 W++;
-document.getElementById("Math").innerHTML="5++"+simple_Math;
-}
-function increment_Operator_Return_Function() {
-    return W;
+document.getElementById("increment_Operator").innerHTML= W;
 }
 var Y = 8;
 Y++;
 document.write(Y);
-
 window.alert(Math.random() * 100);
+
+function Power() {
+    var a = Math.pow(5,3);
+    var x = a
+    document.getElementById("Power").innerHTML=x;
+}
