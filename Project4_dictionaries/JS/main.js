a3d16290a74e5587395a723253f64ddd6fe423bd
brandonlyon24 <72480983+brandonlyon24@users.noreply.github.com>
2020-10-23 18:03:34 -0700

    Update main.js

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -6,9 +6,7 @@ function my_Dictionary() {
         Year:2009,
         Engine:"EJ255",
         Transmission:"Manual",
-        Transmission1:"Auto",
-
     };
-    delete Car.Transmission1;
-    document.getElementById("Dictionary").innerHTML= Car.Brand +Car.Modle +Car.Year;
+    delete Car.Brand; //This deletes transmission
+    document.getElementById("Dictionary").innerHTML= Car.Brand +Car.Transmission +Car.Modle +Car.Year;
 }
